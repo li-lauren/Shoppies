11dfb80479e39a2b08f596732eac6522c39c638c
li-lauren <laurenlidev@gmail.com>
2020-12-19 21:40:29 +0000

    Add nomination model

--- model.py
+++ model.py
@@ -30,6 +30,29 @@ class User(db.Model):
         return f'<User user_id={self.user_id} email={self.email}>'
 
 
+@dataclass
+class Nomination(db.Model):
+    """A Shoppie Nomination."""
+    nomination_id: int
+    title: str
+    release_year: str
+    nominator: int
+    
+    __tablename__ = "nominations"
+
+    nomination_id = db.Column(db.Integer, 
+                              autoincrement=True, 
+                              primary_key=True)
+    title: db.Column(db.String, nullable=False)
+    release_year: db.Column(db.String)
+
+    # id of user who nominated the movie
+    nominator: db.Column(db.Integer, db.ForeignKey("users.user_id"))
+
+    def __repr__(self):
+        return f"<Nomination nomination_id={self.nomination_id} title={self.title}>"
+
+
 def connect_to_db(flask_app, db_uri='postgresql:///shoppies_db', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     flask_app.config['SQLALCHEMY_ECHO'] = echo
