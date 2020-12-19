aebbbcc5e0762b8938f85d7b40e06047ac29fffc
li-lauren <laurenlidev@gmail.com>
2020-12-19 22:57:44 +0000

    Link to backend and implement login form

---
+++ static/js/Login.jsx
@@ -0,0 +1,71 @@
+// Login form
+
+const Login = ({setLoggedIn}) => {
+    const [userInput, setUserInput] = useReducer(
+        (state, newState) => ({...state, ...newState}),
+        {
+            email: '', 
+            password: ''
+        }
+    );
+
+    const [errorMsg, setErrorMsg] = useState('');
+
+    const handleChange = e => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setUserInput({[name]: value});
+    };
+
+    const login = e => {
+        e.preventDefault();
+
+        const reqOptions = {
+            method: 'POST', 
+            headers: {
+                'Content-Type':'application/json'
+            }, 
+            body: JSON.stringify({
+                'email': userInput.email,
+                'password': userInput.password
+            })
+        };
+
+        fetch('/login', reqOptions)
+        .then(res => res.json())
+        .then(data => {
+            if (data.err) {
+                // error occurred during login
+                setErrorMsg(data.err);
+            } else {
+                // successful login
+                setLoggedIn(true);
+            }
+        });
+    };
+
+    return(
+        <div>
+            <form onSubmit={login}>
+                <input 
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    value={userInput.email}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    placeholder="Password"
+                    name="password"
+                    value={userInput.password}
+                    onChange={handleChange}
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+
+
+
+}
