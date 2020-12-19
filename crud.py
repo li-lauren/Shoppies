2fa362b89927ed23e4ff92d4b7d2de4d1133d65e
li-lauren <laurenlidev@gmail.com>
2020-12-19 23:16:56 +0000

    Import schemas and implement fn for querying users by email

--- crud.py
+++ crud.py
@@ -1,9 +1,15 @@
 """CRUD operations."""
 
-from model import db, connect_to_db
+from model import db, connect_to_db, User, Nomination
 
 import os
 
+### USER CRUD OPS ###
+
+def get_user_by_email(email):
+    """Return a user by email."""
+
+    return User.query.filter(User.email == email).first()
 
 if __name__ == '__main__':
     from server import app
