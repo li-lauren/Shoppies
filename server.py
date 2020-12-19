dcf90c8cd8e1fe5ac51ea930c4f6b5fa3400e85f
li-lauren <laurenlidev@gmail.com>
2020-12-19 23:13:22 +0000

    Add /login route

--- server.py
+++ server.py
@@ -12,7 +12,39 @@ app.secret_key = FLASK_SECRET_KEY
 
 @app.route('/')
 def index():
+    """Home page."""
     return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    """Verify credentials and login a user."""
+
+    email = request.json.get("email")
+    password = request.json.get("password")
+
+    user = crud.get_user_by_email(email)
+
+    login_results = {
+        "user": None, 
+        "err": ""
+    }
+
+    if user:
+        if user.password == password:
+            # user is verified, add to flask session
+            session["user_id"] = user.user_id
+            login_results["user"] = {
+                "fname" : user.fname,
+                "lname" : user.lname, 
+                "user_id" : user.user_id
+            }
+        else:
+            login_results["error"] = "Incorrect password. Please try again."
+    else:
+        # No user found in DB
+        login_results["error"] = "No user associated with that email."
+        
 
 if __name__ == '__main__':
     connect_to_db(app)
