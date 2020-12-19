475c878310bb7fe7d4bbae799f568bf1852ca301
li-lauren <laurenlidev@gmail.com>
2020-12-19 21:29:42 +0000

    Add user model

--- model.py
+++ model.py
@@ -5,6 +5,31 @@ from dataclasses import dataclass
 
 db = SQLAlchemy()
 
+@dataclass
+class User(db.Model):
+    """A user."""
+    user_id: int
+    fname: str
+    lname: str
+    email: str
+    pw: str
+    img: str
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, 
+                        autoincrement=True,
+                        primary_key=True)
+
+    fname = db.Column(db.String(50), nullable=False)
+    lname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    pw = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+
+
 def connect_to_db(flask_app, db_uri='postgresql:///shoppies_db', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     flask_app.config['SQLALCHEMY_ECHO'] = echo
