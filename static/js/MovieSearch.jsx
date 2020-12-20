f993c52e62cd95f50e3f02c2741b00d233bee57e
li-lauren <laurenlidev@gmail.com>
2020-12-20 22:08:04 +0000

    Initialize MovieSearch component

---
+++ static/js/MovieSearch.jsx
@@ -0,0 +1,16 @@
+const MovieSearch = () => {
+
+    
+    return (
+        <div>
+            {/* Search Bar */}
+            <form action="">
+                <input type="text"/>
+                <input type="submit" style={{display: "none"}}/>
+            </form>
+
+            {/* Movie Search Results */}
+            
+        </div>
+    )
+}
