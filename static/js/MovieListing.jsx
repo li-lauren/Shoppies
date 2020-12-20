55298850768b0fced7b1c929ce12f06537700583
li-lauren <laurenlidev@gmail.com>
2020-12-20 23:42:36 +0000

    Add nominate and unnominate features

--- static/js/MovieListing.jsx
+++ static/js/MovieListing.jsx
@@ -1,4 +1,29 @@
+// Individual movie listing for a movie search result
+
 const MovieListing = ({movie}) => {
+    const [nominated, setNominated] = useState(false);
+    const imdbID = movie.imdbID;
+
+    useEffect(() => {
+        // initially set 'nominated' var. based on if movie has been nominated 
+        // (i.e. if movie is already in local storage)
+
+        setNominated(localStorage.getItem(imdbID) !== null);
+    }, []);
+
+    const nominateMovie = () => {
+        setNominated(true);
+
+        // save nominated movie to local storage
+        localStorage.setItem(movie.imdbID, movie.Title);
+    };
+
+    const unnominateMovie = () => {
+        setNominated(false);
+
+        // remove movie from local storage
+        localStorage.removeItem(movie.imdbID);
+    };
 
     return (
         <div>
@@ -7,6 +32,14 @@ const MovieListing = ({movie}) => {
                 <img src={movie.Poster} alt={`${movie.Title}-poster`} />
             }
             <span>{movie.Title} ({movie.Year})</span>
+
+            { nominated ?
+                <div>
+                    <button disabled>Nominated</button>
+                    <button onClick={unnominateMovie}>Trash</button>
+                </div> : 
+                <button onClick={nominateMovie}>Nominate</button>
+            }
         </div>
     )
 }
