971fda3f4009a6788f828daaa5679304ce247e65
li-lauren <laurenlidev@gmail.com>
2020-12-20 23:50:03 +0000

    Send nominated IDs to backend

--- static/js/Logout.jsx
+++ static/js/Logout.jsx
@@ -1,15 +1,28 @@
+// Logout button
+
 const Logout = ({setLoggedIn}) => {
+
     const logout = () => {
-        fetch('/logout')
+
+        const reqOptions = {
+            method: 'POST', 
+            headers: {
+                'Content-Type':'application/json'
+            }, 
+            body: JSON.stringify(localStorage.getItem('nominatedMovieIDs'))
+        };
+        
+        fetch('/logout', reqOptions)
         .then(res => res.text())
         .then(data => {
             if (data === 'success') {
-                setLoggedIn(false)
+                setLoggedIn(false);
+                localStorage.clear();
             }
-        })
-    }
-    
+        });
+    };
+
     return(
         <button onClick={logout}>Logout</button>
-    )
+    );
 }
