975a3fbc366028bcf793f9deffc82c988a579f5e
li-lauren <laurenlidev@gmail.com>
2020-12-21 22:06:21 +0000

    Add Nominations.jsx

---
+++ static/js/Nominations.jsx
@@ -0,0 +1,22 @@
+// List of all current nominations
+
+const Nominations = () => {
+    const [nominations, setNominations] = useState(null);
+
+    useEffect(() => {
+        getNominations();
+    }, []);
+
+    const getNominations = () => {
+        fetch('/nominations')
+        .then(res => res.json())
+        .then(data => setNominations(data));
+    };
+
+    return (
+        <div>
+            <span>Your Nominations</span>
+            {nominations ? nominations.map(nomination => console.log(nomination)) : ''}
+        </div>
+    );
+}
