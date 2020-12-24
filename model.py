ee3ee8b6bc1ad84afca5565e4a967b6007831aa8
li-lauren <laurenlidev@gmail.com>
2020-12-24 00:44:56 +0000

    Add app context

--- model.py
+++ model.py
@@ -66,6 +66,9 @@ def connect_to_db(flask_app, db_uri=None, echo=True):
     db.app = flask_app
     db.init_app(flask_app)
 
+    with flask_app.app_context():
+        db.create_all()
+
     print('Connected to the db!')
 
 if __name__ == '__main__':
